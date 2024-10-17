8a80d14ac5903a7d11474cda4ec45fbc25b44c87
santosestefani <142347589+santosestefani@users.noreply.github.com>
2024-10-17 16:38:03 -0300

    24

--- perguntas.js
+++ perguntas.js
@@ -1,6 +1,6 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
+    'O que é gato?',
     'O Python é uma linguagem de programação'
 )
 
@@ -10,6 +10,17 @@ criaCartao(
     'A capital da França é Paris'
 )
 
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 criaCartao(
     'Programação',
     'O que é uma função?',
